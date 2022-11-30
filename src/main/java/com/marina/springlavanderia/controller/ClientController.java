0978ea623f5df6ed7d7525040dc5853707a360dc
wiliangulini <gulini.dev@gmail.com>
2022-11-30 10:18:32 -0300

    httpstatus.created inserido

--- src/main/java/com/marina/springlavanderia/controller/ClientController.java
+++ src/main/java/com/marina/springlavanderia/controller/ClientController.java
@@ -3,6 +3,8 @@ package com.marina.springlavanderia.controller;
 import com.marina.springlavanderia.model.Client;
 import com.marina.springlavanderia.repository.ClientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,10 +21,12 @@ public class ClientController {
   }
 
   @PostMapping
-  public void create(@RequestBody Client client) {
+  @ResponseStatus(HttpStatus.CREATED)
+  public Client create(@RequestBody Client client) {
     System.out.println(client.getCliente());
     System.out.println(client.getRua());
 
-    clientRepository.save(client);
+    return clientRepository.save(client);
+//    return ResponseEntity.status(HttpStatus.CREATED).body(clientRepository.save(client)); nesse caso em particular do create como nao estamos manuseando response, nao estamos adicionando cabecalho, O RESPONSESTATUS, é uma alternativa do ResponseEntity, a vantagem de usar o responseentity é caso vc precise manusear header, ou informacoes do response podemos fazer isso, mas como so queremos o status e nao vamos manusear o response, ai podemos declarar a anotacao @ResponseStatus
   }
 }
