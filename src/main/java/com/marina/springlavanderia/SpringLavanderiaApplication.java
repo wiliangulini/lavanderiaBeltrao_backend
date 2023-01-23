a433f881be2e2fb1dd9a1290809163833225a932
wiliangulini <gulini.dev@gmail.com>
2023-01-23 02:39:52 -0300

    finalizacao de projeto spring

--- src/main/java/com/marina/springlavanderia/SpringLavanderiaApplication.java
+++ src/main/java/com/marina/springlavanderia/SpringLavanderiaApplication.java
@@ -2,19 +2,11 @@ package com.marina.springlavanderia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-// extends SpringBootServletInitializer
 @SpringBootApplication
 public class SpringLavanderiaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringLavanderiaApplication.class, args);
   }
-
-//  @Override
-//  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//    return application.sources(SpringLavanderiaApplication.class);
-//  }
 
 }
---
+++ src/main/java/com/marina/springlavanderia/configs/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.marina.springlavanderia.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration(proxyBeanMethods = false)
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+      }
+
+    };
+  }
+
+}
+
