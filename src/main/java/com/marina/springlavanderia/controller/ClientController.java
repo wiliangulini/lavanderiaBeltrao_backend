72c9d9261bdecb4515da011f8015e1531c1a4bc5
Wilian Gulini <wilian.gulini@codengage.com>
2025-10-26 22:50:56 -0300

    ediçao e registro de pedidos funcionando com sucesso

--- src/main/java/com/marina/springlavanderia/controller/ClientController.java
+++ src/main/java/com/marina/springlavanderia/controller/ClientController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping("/api/clientes")
 public class ClientController {
 
     private final ClientService clientService;
